df1f8c0ea7f497132ce2e764b66fca71fab663e0
Kaique62 <kaiquegm623@gmail.com>
2024-08-05 15:35:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ app.title = "Controle"
 app.geometry("520x1080")
 
 notebook = ttk.Notebook(app, width="520", height="1080")
-notebook.grid()
-notebook.grid_propagate(False)
+notebook.pack()
+notebook.pack_propagate(False)
 
-frameAlunos = Frame(notebook, width="260")
-frameAlunos.grid()
-frameAlunos.grid_propagate(False)
+frameAlunos = Frame(notebook, width="260", height="1080")
+frameAlunos.pack()
+frameAlunos.pack_propagate(False)
 
-frameAgenda = Frame(notebook, width="260")
-frameAgenda.grid()
-frameAgenda.grid_propagate(False)
+frameAgenda = Frame(notebook, width="260",  height="1080")
+frameAgenda.pack()
+frameAgenda.pack_propagate(False)
 
 notebook.add(frameAlunos, text="Alunos")
 notebook.add(frameAgenda, text="Agenda")
 
-print("teste")
-
-app.mainloop();
+app.mainloop()
