5d1aaa9e98cf3effe508604d9be3de90fe15d3bf
Kaique62 <kaiquegm623@gmail.com>
2024-08-05 15:22:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 
-app = Tk();
-app.title = "Controle";
-app.geometry("520x1080");
+app = Tk()
+app.title = "Controle"
+app.geometry("520x1080")
 
-notebook = ttk.Notebook(app, width="520", height="1080");
-notebook.grid();
+notebook = ttk.Notebook(app, width="520", height="1080")
+notebook.grid()
+notebook.grid_propagate(False)
 
-frameAlunos = Frame(notebook, width="260");
-frameAlunos.grid();
+frameAlunos = Frame(notebook, width="260")
+frameAlunos.grid()
+frameAlunos.grid_propagate(False)
 
-frameAgenda = Frame(notebook, width="260");
-frameAgenda.grid();
+frameAgenda = Frame(notebook, width="260")
+frameAgenda.grid()
+frameAgenda.grid_propagate(False)
 
 notebook.add(frameAlunos, text="Alunos")
 notebook.add(frameAgenda, text="Agenda")
