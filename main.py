fb50570a706b7e4febff99102cd1207084e38ec1
Kaique62 <kaiquegm623@gmail.com>
2024-08-07 15:27:03 -0300

    Parte de Cadastro Feita

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from tkinter import ttk
 
 frames = {}
 
-alunos = {0: {"Nome": "Kaique Madureira", "Status de Pagamento": "Pago", "Data de Vencimento": '06/08/2024', "Idade":16, "Altura": 162, "Peso": 47, "Gênero": "M"}, 1: {"Nome": "Bruna"}}
+#alunos = [{"Nome": "Kaique Madureira", "Idade":'16 anos', "Altura": '162 cm', "Peso": '47 KG', "Gênero": "M"}, {"Nome": "Bruna"}]
+alunos = []
 
 class App(Tk):
     def __init__(self):
@@ -12,7 +13,7 @@ class App(Tk):
         self.title = "Controle"
         self.geometry("1080x1080")
         
-        self.infoLabels = ["Nome", "Status de Pagamento", "Data de Vencimento", "Idade"]
+        self.infoLabels = ['Nome', 'Idade', 'Altura', 'Peso']
         self.FramesDict = {}
         self.labelsDict = {}
         self.labelsValue = {}
@@ -21,6 +22,8 @@ class App(Tk):
 
         self.curPage = 0
         self.maxPage = (len(alunos)/8)
+        if len(alunos)%8 == 0 and not self.maxPage == 0:
+            self.maxPage = (len(alunos)/8) - 1
 
         self.startup()
     
@@ -48,11 +51,11 @@ class App(Tk):
     
     def newInfoFrame(self):
         
-        self.topFrame = Frame(self.frameAlunos, width="1080", height="180", highlightthickness=1, highlightbackground="black")
+        self.topFrame = Frame(self.frameAlunos, width="1080", height="140", highlightthickness=1, highlightbackground="black")
         self.topFrame.pack(anchor=N)
         self.topFrame.pack_propagate(False)
         
-        self.leftArrowFrame = Frame(self.topFrame, width=100, height=180, highlightthickness=1, highlightbackground="blue")
+        self.leftArrowFrame = Frame(self.topFrame, width=100, height=140, highlightthickness=1, highlightbackground="blue")
         self.leftArrowFrame.grid(sticky=W, column=0, row=0)
         self.leftArrowFrame.bind("<Button>", lambda e: self.movePage(-1))
         self.leftArrowLabel = Label(self.leftArrowFrame, text="←", font=("Arial", 40), background="blue", fg="white")
@@ -64,14 +67,23 @@ class App(Tk):
         self.pageLabel.grid(sticky=N, column=1, row=0, padx=445)
         self.pageLabelString.set(f"Página\n{self.curPage}/{self.maxPage:,.0f}")
         
-        self.RightArrowFrame = Frame(self.topFrame, width=100, height=180, highlightthickness=1, highlightbackground="blue")
+        self.RightArrowFrame = Frame(self.topFrame, width=100, height=140, highlightthickness=1, highlightbackground="blue")
         self.RightArrowFrame.grid(sticky=W, column=2, row=0)
         self.RightArrowFrame.bind("<Button>", lambda e: self.movePage(1))
         self.RightArrowLabel = Label(self.RightArrowFrame, text="→", font=("Arial", 40), background="blue", fg="white")
         self.RightArrowLabel.bind("<Button>", lambda e: self.movePage(1))
         self.RightArrowLabel.grid(sticky=N)
         
-        for num in range(0 + (9*self.curPage), 8 + (8*self.curPage)):  
+        self.addButton = Frame(self.frameAlunos, width="1080", height="50", highlightthickness=1, highlightbackground="black")
+        self.addButton.pack(anchor=N, pady=3)
+        self.addButton.pack_propagate(False)
+        self.addButton.bind('<Button>', lambda e: self.telaDeCadastro())
+        
+        self.addButtonLabel = Label(self.addButton, text="(+) Adicionar", font=("Arial", 14))
+        self.addButtonLabel.pack(anchor=N, pady=5)
+        self.addButtonLabel.bind('<Button>', lambda e: self.telaDeCadastro())
+        
+        for num in range(0 + (8*self.curPage), 8 + (8*self.curPage)):  
             try:
                 alunos[num]
                 self.FramesDict[num] = Frame(self.frameAlunos, width="1080", height="100", highlightthickness=1, highlightbackground="black")
@@ -91,8 +103,11 @@ class App(Tk):
         
     def refreshMainPage(self):
         self.clear()
-        self.startup()
+        self.maxPage = (len(alunos)/8)
+        if len(alunos)%8 == 0 and not self.maxPage == 0:
+            self.maxPage = (len(alunos)/8) - 1
         self.pageLabelString.set(f"Página\n{self.curPage}/{self.maxPage:,.0f}")
+        self.startup()
         
     def movePage(self, num):
         if self.curPage != 0 and num == -1:
@@ -105,25 +120,88 @@ class App(Tk):
     def openInfo(self, aluno):
         self.clear()
           
-        self.nameFrame = LabelFrame(self, width=240, height=60, text="Nome", font=("Arial", "12"))
-        self.nameFrame.grid(pady=3, sticky=W, padx=3)
-        self.nameFrame.grid_propagate(False)
+        self.info = ['Nome', 'Idade', 'Altura', 'Peso', 'Gênero']
+        self.openInfoFrames = {}
+        self.openInfoEntry = {}
+        self.openInfoValues = {}
         
-        self.nameString = StringVar()
-        self.nameLabel = Entry(self.nameFrame, textvariable=self.nameString, font=("Arial", "15"), bg=self.cget("bg"), borderwidth=0)   
-        self.nameLabel.grid(column=0, row=0, sticky=W)
-        self.nameLabel.grid_propagate(False)
-        self.nameString.set(alunos[aluno]['Nome']) 
+        self.counter = 0
+        self.column = 0
         
-        self.idadeFrame = LabelFrame(self, width=240, height=60, text="Idade", font=("Arial", "12"))
-        self.idadeFrame.grid(pady=3, sticky=W, padx=3)
-        self.idadeFrame.grid_propagate(False)
+        for i in self.info:
+            self.openInfoFrames[i] = LabelFrame(self, width=240, height=60, text=i, font=("Arial", "12"))
+            self.openInfoFrames[i].grid(pady=3, sticky=W, padx=3, column=self.column, row=self.counter)
+            self.openInfoFrames[i].grid_propagate(False)
+            
+            self.openInfoValues[i] = StringVar()
+            self.openInfoEntry[i] = Entry(self.openInfoFrames[i], textvariable=self.openInfoValues[i], font=("Arial", "15"), bg=self.cget("bg"), borderwidth=0) 
+            self.openInfoEntry[i].grid(sticky=W)
+            self.openInfoEntry[i].grid_propagate(False)
+            self.openInfoValues[i].set(alunos[aluno][i]) 
+            
+            self.counter += 1
+            if self.counter == 2:
+                self.column += 1
+                self.counter = 0
+                
+        self.saveButton = Button(self, text="Salvar", width=10, height=2, font=("Arial", "12"), command= lambda a=aluno: self.substituirInfo(a))
+        self.saveButton.grid(column=3, row=0, pady=5)
         
-        self.idadeString = StringVar()
-        self.idadeLabel = Entry(self.idadeFrame, textvariable=self.idadeString, font=("Arial", "15"), bg=self.cget("bg"), borderwidth=0)   
-        self.idadeLabel.grid(column=0, row=0, sticky=W)
-        self.idadeLabel.grid_propagate(False)
-        self.idadeString.set(alunos[aluno]['Idade']) 
+        self.closeButton = Button(self, text="Cancelar", width=10, height=2, font=("Arial", "12"), command=self.refreshMainPage)
+        self.closeButton.grid(column=4, row=0, pady=5)
+    
+    def substituirInfo(self, aluno):
+        alunos[aluno] = {"Nome": self.openInfoEntry["Nome"].get(),
+                        "Idade": self.openInfoEntry["Idade"].get(),
+                        "Peso": self.openInfoEntry["Peso"].get(),
+                        "Altura": self.openInfoEntry["Altura"].get(),
+                        "Gênero": self.openInfoEntry["Gênero"].get()}
+        
+        
+    def telaDeCadastro(self):
+        self.clear()
+        self.infoCadastro = ['Nome', 'Idade', 'Peso','Altura', 'Gênero']
+        self.cadastroLabel = {}
+        self.cadastroEntry = {}
+        
+        self.cadastroFrame = Frame(self, width=300, height=520, highlightthickness=1, highlightbackground="black")
+        self.cadastroFrame.pack(anchor=CENTER, pady=100)
+        self.cadastroFrame.pack_propagate(False)
+        
+        for i in self.infoCadastro:
+            self.cadastroLabel[i] = Label(self.cadastroFrame, text=i, font=("Arial", '16'))
+            self.cadastroLabel[i].pack(anchor=W, padx=25)
+            
+            self.cadastroEntry[i] = Entry(self.cadastroFrame, font=("Arial", '16'))
+            self.cadastroEntry[i].pack()
+            
+        self.errorLabel = Label(self.cadastroFrame, text="Preencha Todos os Campos!", fg=self.cget("bg"), font=("Arial", '16'))
+        self.errorLabel.pack(pady=5)
+            
+        self.buttonsFrame = Frame(self.cadastroFrame)
+        self.buttonsFrame.pack(pady=40)
+            
+        self.saveButton = Button(self.buttonsFrame, text="Salvar", font=("Arial", '16'), width=10, command=self.salvar)
+        self.saveButton.pack(anchor=S, pady=5)
+        
+        self.cancelButton = Button(self.buttonsFrame, text="Cancelar", font=("Arial", '16'), width=10, command=self.refreshMainPage)
+        self.cancelButton.pack(anchor=S, pady=5)
+        
+    def salvar(self):
+        self.canSave = True
+        
+        for i in self.cadastroEntry:
+            if self.cadastroEntry[i].get() == '':
+                self.canSave = False
+        if self.canSave:
+            alunos.append({"Nome": self.cadastroEntry["Nome"].get(),
+                        "Idade": self.cadastroEntry["Idade"].get(),
+                        "Peso": self.cadastroEntry["Peso"].get(),
+                        "Altura": self.cadastroEntry["Altura"].get(),
+                        "Gênero": self.cadastroEntry["Gênero"].get()})
+            self.refreshMainPage()
+        else:
+            self.errorLabel.config(fg="red")
         
 if __name__ == "__main__":
     root = App()
